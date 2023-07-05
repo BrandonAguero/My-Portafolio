f0eb156ba90e29cec8e6fd1d110a0143fccd73d9
Brandon <name.brandon.aguero@gmail.com>
2023-07-04 21:51:29 -0500

    Cree la carpeta components y dentro de esta cree la carpeta shared. A su vez cree el componente Header.jsx

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>El header</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

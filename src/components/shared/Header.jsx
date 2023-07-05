3845059c5c82e3fa74b8141e43a3ad6b23f756c0
Brandon <name.brandon.aguero@gmail.com>
2023-07-05 07:55:56 -0500

    Reemplaze los Link por NavLink para ofrecer dinamismo en la selección de la rutas. Modifique el w-screen por w-full en index

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,34 +1,89 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className="flex h-auto w-full">
+      <nav className="flex w-full items-center justify-between">
         <ul>
           <li>
-            <Link to="/">
+            <NavLink to="/" caseSensitive>
               <img
                 className="h-44 w-44 object-contain"
                 src="/png/logoHome.png"
               />
-            </Link>
+            </NavLink>
           </li>
         </ul>
         <ul className="flex gap-2">
-          <li className="inline-flex cursor-pointer rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600">
-            <Link to="/about-me">Acerca de mí</Link>
+          <li>
+            <NavLink
+              to="/about-me"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? ""
+                  : isActive
+                  ? "flex w-36 cursor-pointer justify-center rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+                  : "flex w-36 cursor-pointer justify-center rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50 hover:bg-gradient-to-tr active:border-b-green-800 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+              }
+            >
+              Acerca de mí
+            </NavLink>
           </li>
-          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
-            <Link to="/projects">Proyectos</Link>
+          <li>
+            <NavLink
+              to="/projects"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? ""
+                  : isActive
+                  ? "flex w-36 cursor-pointer justify-center rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+                  : "flex w-36 cursor-pointer justify-center rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50 active:border-b-green-800 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+              }
+            >
+              Proyectos
+            </NavLink>
           </li>
-          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
-            <Link to="/contact-me">Contáctame</Link>
+          <li>
+            <NavLink
+              to="/contact-me"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? ""
+                  : isActive
+                  ? "flex w-36 cursor-pointer justify-center rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+                  : "flex w-36 cursor-pointer justify-center rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50 active:border-b-green-800 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+              }
+            >
+              Contáctame
+            </NavLink>
           </li>
-          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
-            <Link to="/education">Educación</Link>
+          <li>
+            <NavLink
+              to="/education"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? ""
+                  : isActive
+                  ? "flex w-36 cursor-pointer justify-center rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+                  : "flex w-36 cursor-pointer justify-center rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50 active:border-b-green-800 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+              }
+            >
+              Educación
+            </NavLink>
           </li>
-          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
-            <Link to="/skills">Habilidades</Link>
+          <li>
+            <NavLink
+              to="/skills"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? ""
+                  : isActive
+                  ? "flex w-36 cursor-pointer justify-center rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+                  : "flex w-36 cursor-pointer justify-center rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50 active:border-b-green-800 active:bg-gradient-to-tr active:from-green-500 active:to-green-600"
+              }
+            >
+              Habilidades
+            </NavLink>
           </li>
         </ul>
       </nav>

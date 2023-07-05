64004539a07396307b946900ce61262c60cf6146
Brandon <name.brandon.aguero@gmail.com>
2023-07-05 00:08:56 -0500

    Añadí estilos a todas las etiquetas li. También cambie la imagen logoHome por una que combine mucho mejor con los colores usados en las etiquetas li

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -14,20 +14,20 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <ul>
-          <li>
+        <ul className="flex gap-2">
+          <li className="inline-flex cursor-pointer rounded-lg border-b-4 border-b-blue-800 bg-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-b-green-900 hover:bg-green-600">
             <Link to="/about-me">Acerca de mí</Link>
           </li>
-          <li>
+          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
             <Link to="/projects">Proyectos</Link>
           </li>
-          <li>
+          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
             <Link to="/contact-me">Contáctame</Link>
           </li>
-          <li>
+          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
             <Link to="/education">Educación</Link>
           </li>
-          <li>
+          <li className="inline-flex cursor-pointer rounded-lg border border-b-4 border-blue-500 p-3 font-serif font-semibold text-gray-900 hover:border-green-600 hover:bg-green-50">
             <Link to="/skills">Habilidades</Link>
           </li>
         </ul>

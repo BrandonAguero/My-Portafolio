853d9e8d90c01a35793ca2f7c8059b64bb749bb4
Brandon <name.brandon.aguero@gmail.com>
2023-07-05 14:00:47 -0500

    Agregue una nueva animación para todos los componentes. Esta animación desaparece el contenido actual y el siguiente contenido lo trae de la derecha

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,17 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+    <motion.main
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+        transition: {
+          duration: 1.5,
+          ease: "easeInOut",
+        },
+      }}
+    >
       <h1>ESte es el home</h1>
     </motion.main>
   );
--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -3,9 +3,15 @@ import { motion } from "framer-motion";
 const SkillsPage = () => {
   return (
     <motion.main
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+        transition: {
+          duration: 1.5,
+          ease: "easeInOut",
+        },
+      }}
     >
       <h1>This is the SkillsPage</h1>
     </motion.main>

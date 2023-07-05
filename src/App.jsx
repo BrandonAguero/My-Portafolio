4200fac1f67d4778fd86b4e72f959ca04888dd21
Brandon <name.brandon.aguero@gmail.com>
2023-07-04 22:16:32 -0500

    Añadi en la carpeta png a logoFooter y logoHome. En el componente Header.jsx que esta en shared, agrege un link que lleva a Home, con la imagen logoHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home.jsx";
+import Header from "./components/shared/Header.jsx";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
       <nav>
         <ul>
-          <li>El header</li>
+          <li>
+            <Link to="/">
+              <img
+                className="h-44 w-44 object-contain"
+                src="/png/logoHome.png"
+              />
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>

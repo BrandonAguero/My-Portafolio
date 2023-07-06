bb90f05da0e8196b80f310ebc5ad3fa3e7903f49
Brandon <name.brandon.aguero@gmail.com>
2023-07-06 11:13:58 -0500

    Reemplaze el top y el padding-top por un min-height en la etiqueta header en el componente Header.jsx

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   console.log(window.scrollY);
 
   return (
-    <header className="flex h-20 w-full">
+    <header className="flex min-h-[8rem] w-full">
       <nav className="fixed top-0 z-10 flex h-24 w-full flex-col justify-center bg-gray-100 xl:flex-row xl:items-center xl:justify-between">
         <ul className="flex w-full items-center justify-between px-2 xl:w-max">
           <li className="flex h-max w-max items-center justify-center">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,19 +12,19 @@ const Home = () => {
           ease: "easeInOut",
         },
       }}
-      className="relative flex flex-col pt-28 small:pt-12"
+      className="relative flex flex-col"
     >
       <div className="flex flex-col-reverse items-center gap-4">
         <section className="flex w-[95%] flex-col">
           <div>
-            <h1 className="flex flex-col font-serif text-3xl text-gray-800 small:text-4xl">
+            <h1 className="flex flex-col font-serif text-3xl font-bold text-gray-800 small:text-4xl super:text-5xl 2md:text-6xl xl:text-7xl">
               <span>Hola,</span>
               <span>soy Brandon Agüero</span>
             </h1>
-            <h2 className="font-serif text-gray-700">
+            <h2 className="font-serif text-xl font-semibold text-gray-700 small:text-2xl super:text-3xl 2md:text-4xl xl:text-5xl">
               Soy un Frontend Developer
             </h2>
-            <p className="font-sans text-gray-600">
+            <p className="font-sans text-base font-normal text-gray-600 small:text-lg super:text-xl 2md:text-2xl xl:text-3xl">
               ¡Bienvenido a mi portafolio! Aquí podrás explorar mis proyectos,
               habilidades y educación. Si estás interesado en colaborar, no
               dudes en contactarme.

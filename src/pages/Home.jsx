1361a8c340929a4292f9899844f8bee90606dc4d
Brandon <name.brandon.aguero@gmail.com>
2023-07-06 15:33:22 -0500

    Cambie el texto de bienvenida por una más corto, agregue una animación con hover para el subrayado de los enlaces. Y mejore el responsive en general de la pagina web para el componente Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
           ease: "easeInOut",
         },
       }}
-      className="relative flex flex-col"
+      className="flex flex-col"
     >
       <div className="flex flex-col-reverse items-center gap-4 md:flex-row md:px-4 lg:px-6 xl:px-8">
-        <section className="flex w-[95%] flex-col">
-          <div className="flex flex-col gap-2">
+        <section className="flex w-[95%] flex-col gap-2 medium:gap-4 super:gap-6 2md:gap-8">
+          <div className="flex flex-col gap-2 super:gap-4">
             <h1 className="flex flex-col font-serif text-3xl font-bold text-gray-800 small:text-4xl super:text-5xl 2md:text-6xl xl:text-7xl">
               <span>Hola,</span>
               <span>soy Brandon Agüero</span>
@@ -25,44 +25,65 @@ const Home = () => {
               Frontend Developer
             </h2>
             <p className="font-sans text-base font-normal text-gray-600 small:text-lg super:text-xl 2md:text-2xl xl:text-3xl">
-              ¡Bienvenido a mi portafolio! Aquí podrás explorar mis proyectos,
-              habilidades y educación. Si estás interesado en colaborar, no
-              dudes en contactarme.
+              ¡Bienvenido a mi página web! Soy estudiante de Academlo. Explora
+              mis proyectos y habilidades, y no dudes en contactarme para
+              colaborar en emocionantes proyectos.
             </p>
           </div>
-          <footer>
-            <ul className="flex gap-4">
-              <li className="">
+          <footer className="flex justify-center md:justify-start">
+            <ul className="flex w-[93%] flex-wrap justify-center gap-4 max-large:max-w-[15rem] super:gap-6 md:justify-start">
+              <li className="text-base">
                 <a
                   href="https://www.linkedin.com/in/brandonaguero/"
                   target="_blank"
+                  className="group relative inline-flex items-center gap-1 font-mono"
                 >
-                  <i className="bx bxl-linkedin bx-tada text-blue-500"></i>{" "}
-                  LinkedIn
+                  <i className="bx bxl-linkedin bx-tada text-blue-500"></i>
+                  <span className="text-base text-gray-600 small:text-xl">
+                    LinkedIn
+                  </span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 w-full origin-left scale-x-0 transform bg-blue-500 transition-transform duration-300 group-hover:scale-x-100"></span>
                 </a>
               </li>
-              <li>
-                <a href="https://github.com/BrandonAguero" target="_blank">
-                  <i className="bx bxl-github bx-tada text-gray-800"></i>Github
+              <li className="text-base">
+                <a
+                  href="https://github.com/BrandonAguero"
+                  target="_blank"
+                  className="group relative inline-flex items-center gap-1 font-mono"
+                >
+                  <i className="bx bxl-github bx-tada text-gray-800"></i>
+                  <span className="text-base text-gray-600 small:text-xl">
+                    Github
+                  </span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 w-full origin-left scale-x-0 transform bg-gray-800 transition-transform duration-300 group-hover:scale-x-100"></span>
                 </a>
               </li>
-              <li>
+              <li className="text-base">
                 <a
                   href="https://api.whatsapp.com/send?phone=+51987398362&text=Hola,%20soy..."
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="group relative inline-flex items-center gap-1 font-mono"
                 >
                   <i className="bx bxl-whatsapp bx-tada text-green-500"></i>
-                  WhatsApp
+                  <span className="text-base text-gray-600 small:text-xl">
+                    WhatsApp
+                  </span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 w-full origin-left scale-x-0 transform bg-green-500 transition-transform duration-300 group-hover:scale-x-100"></span>
                 </a>
               </li>
-              <li>
+              <li className="text-base">
                 <a
                   href="mailto:name.brandon.aguero@gmail.com"
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="group relative inline-flex items-center gap-1 font-mono"
                 >
-                  <i className="bx bxl-gmail bx-tada text-red-600"></i> Gmail
+                  <i className="bx bxl-gmail bx-tada text-red-600"></i>
+                  <span className="text-base text-gray-600 small:text-xl">
+                    Gmail
+                  </span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 w-full origin-left scale-x-0 transform bg-red-600 transition-transform duration-300 group-hover:scale-x-100"></span>
                 </a>
               </li>
             </ul>

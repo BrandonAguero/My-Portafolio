de59ba996baabcd45241856aece0d244b7d75368
Brandon <name.brandon.aguero@gmail.com>
2023-07-06 11:58:22 -0500

    Agregue una animación para los iconos y colores representativos para cada icono

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,15 +14,15 @@ const Home = () => {
       }}
       className="relative flex flex-col"
     >
-      <div className="flex flex-col-reverse items-center gap-4">
+      <div className="flex flex-col-reverse items-center gap-4 md:flex-row md:px-4 lg:px-6 xl:px-8">
         <section className="flex w-[95%] flex-col">
-          <div>
+          <div className="flex flex-col gap-2">
             <h1 className="flex flex-col font-serif text-3xl font-bold text-gray-800 small:text-4xl super:text-5xl 2md:text-6xl xl:text-7xl">
               <span>Hola,</span>
               <span>soy Brandon Agüero</span>
             </h1>
             <h2 className="font-serif text-xl font-semibold text-gray-700 small:text-2xl super:text-3xl 2md:text-4xl xl:text-5xl">
-              Soy un Frontend Developer
+              Frontend Developer
             </h2>
             <p className="font-sans text-base font-normal text-gray-600 small:text-lg super:text-xl 2md:text-2xl xl:text-3xl">
               ¡Bienvenido a mi portafolio! Aquí podrás explorar mis proyectos,
@@ -31,18 +31,19 @@ const Home = () => {
             </p>
           </div>
           <footer>
-            <ul>
-              <li>
+            <ul className="flex gap-4">
+              <li className="">
                 <a
                   href="https://www.linkedin.com/in/brandonaguero/"
                   target="_blank"
                 >
+                  <i className="bx bxl-linkedin bx-tada text-blue-500"></i>{" "}
                   LinkedIn
                 </a>
               </li>
               <li>
                 <a href="https://github.com/BrandonAguero" target="_blank">
-                  Github
+                  <i className="bx bxl-github bx-tada text-gray-800"></i>Github
                 </a>
               </li>
               <li>
@@ -51,6 +52,7 @@ const Home = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
+                  <i className="bx bxl-whatsapp bx-tada text-green-500"></i>
                   WhatsApp
                 </a>
               </li>
@@ -60,14 +62,14 @@ const Home = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Gmail
+                  <i className="bx bxl-gmail bx-tada text-red-600"></i> Gmail
                 </a>
               </li>
             </ul>
           </footer>
         </section>
         <motion.figure
-          className="h-44 w-44 max-xl:rounded-full small:h-60 small:w-60"
+          className="h-44 w-44 max-xl:rounded-full small:h-60 small:w-60 super:h-72 super:w-72 md:rounded-xl  2md:h-96 2md:w-96 xl:h-[30rem] xl:w-[30rem]"
           initial={{ scale: 0.8 }}
           animate={{
             scale: 1,
@@ -80,7 +82,7 @@ const Home = () => {
           whileHover={{ scale: 1.05 }}
         >
           <motion.img
-            className="h-full w-full rounded-full object-cover object-top"
+            className="h-full w-full rounded-full object-cover object-top md:rounded-xl"
             src="/png/profileHomeTwo.png"
             alt="Foto de perfil del creador de la página web"
           />
